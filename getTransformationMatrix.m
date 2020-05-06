679c4a017691d41c4ef8d3e435311d6bc602fc72
Valdir Jr <vsj@.ic.ufal.br>
2020-05-06 20:03:19 -0300

    Finalizacao

--- getTransformationMatrix.m
+++ getTransformationMatrix.m
@@ -1,6 +1,5 @@
-%Local functions
 function Transf = getTransformationMatrix(pos, orient)
-Transf=eye(4,4);
-Transf(1:3,1:3)=orient;
-Transf(1:3,4)=pos;
+    Transf=eye(4,4);
+    Transf(1:3,1:3)=orient;
+    Transf(1:3,4)=pos;
 end
